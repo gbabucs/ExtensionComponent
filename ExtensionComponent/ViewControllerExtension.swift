1586c45157f7ee8e848ddb6980f0ef9b1a3eda06
Babu Gangatharan <gbabucs@gmail.com>
2019-03-24 19:25:25 +0100

    getting ready for use as a pod

--- ExtensionComponent/ViewControllerExtension.swift
+++ ExtensionComponent/ViewControllerExtension.swift
@@ -6,4 +6,21 @@
 //  Copyright © 2019 babu. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+
+public extension UIViewController {
+    
+    var isModal: Bool {
+        return presentingViewController != nil ||
+            navigationController?.presentingViewController?.presentedViewController === navigationController ||
+            tabBarController?.presentingViewController is UITabBarController
+    }
+    
+    public func showAlert() {
+        let alertViewController = UIAlertController(title: "Alert", message: "Sample pod creation", preferredStyle: .alert)
+        
+        self.present(alertViewController, animated: true, completion: nil)
+    }
+    
+}
